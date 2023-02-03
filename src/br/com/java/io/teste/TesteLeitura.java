e31f03fe73a9fe3e965f871c12dda0d4dcb1d333
Pedro Pereira <Pedro.Pereira@wj.com.br>
2023-02-03 11:59:59 -0300

    first commit java-io

---
+++ src/br/com/java/io/teste/TesteLeitura.java
@@ -0,0 +1,20 @@
+package br.com.java.io.teste;
+
+import java.io.*;
+
+public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de Entrada com Arquivos
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        System.out.println(linha);
+
+        br.close();
+
+    }
+}
